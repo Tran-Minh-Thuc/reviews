38d02794afe25bef3642f84cd2014635b6a54686
huynhhung10 <100453652+huynhhung10@users.noreply.github.com>
2024-10-21 20:00:54 +0700

    Add giao diện + Regis/login/Auth

---
+++ migrations/Version20241021101755.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241021101755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE customers (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, img LONGTEXT NOT NULL, email VARCHAR(255) NOT NULL, phonenumber VARCHAR(255) NOT NULL, status SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE genres (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movies (id INT AUTO_INCREMENT NOT NULL, genre_id_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, decription LONGTEXT NOT NULL, img LONGTEXT NOT NULL, createat DATETIME NOT NULL, INDEX IDX_C61EED30C2428192 (genre_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reviews (id INT AUTO_INCREMENT NOT NULL, movie_id_id INT DEFAULT NULL, customer_id_id INT DEFAULT NULL, reviewtext LONGTEXT NOT NULL, rating INT NOT NULL, INDEX IDX_6970EB0F10684CB (movie_id_id), INDEX IDX_6970EB0FB171EB6C (customer_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE movies ADD CONSTRAINT FK_C61EED30C2428192 FOREIGN KEY (genre_id_id) REFERENCES genres (id)');
+        $this->addSql('ALTER TABLE reviews ADD CONSTRAINT FK_6970EB0F10684CB FOREIGN KEY (movie_id_id) REFERENCES movies (id)');
+        $this->addSql('ALTER TABLE reviews ADD CONSTRAINT FK_6970EB0FB171EB6C FOREIGN KEY (customer_id_id) REFERENCES customers (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movies DROP FOREIGN KEY FK_C61EED30C2428192');
+        $this->addSql('ALTER TABLE reviews DROP FOREIGN KEY FK_6970EB0F10684CB');
+        $this->addSql('ALTER TABLE reviews DROP FOREIGN KEY FK_6970EB0FB171EB6C');
+        $this->addSql('DROP TABLE customers');
+        $this->addSql('DROP TABLE genres');
+        $this->addSql('DROP TABLE movies');
+        $this->addSql('DROP TABLE reviews');
+        $this->addSql('DROP TABLE user');
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(['message' => 'không được để trống.']),
+                    new Email(['message' => 'Email không hợp lệ.']),
+                ],
+                'attr' => ['class' => 'text email', 'autocomplete' => 'email', 'placeholder' => 'Email', 'name' => 'email']
+            ])
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
+            ->add('plainPassword', RepeatedType::class, [
+                'label' => false,
+                'mapped' => false,
+                'required' => true,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'attr' => ['autocomplete' => 'new-password', 'name' => 'password', 'class' => 'text'],
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+
+                ],
+                'first_options'  => ['label' => false,  'attr' => ['autocomplete' => 'new-password', 'name' => 'password', 'class' => 'text', 'placeholder' => 'password'],],
+                'second_options' => ['label' => false, 'attr' => ['autocomplete' => 'new-password', 'name' => 'password', 'class' => 'text w3lpass', 'placeholder' => 'repeat password'],],
+
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
